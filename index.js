7115471e5e32085866abedabd36fb666a7bb2a8c
Viet Hung <hung@HungViet.local>
2019-06-22 15:08:13 +0700

    update sever

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 app.use(express.static('public'))
 app.get('/', (req, res) => {
     res.sendFile(__dirname.concat('/public/index.html'))
 })
 app.listen(port, () => {
-    console.log('server is running')
+    console.log('server is running on port '.concat(port, '...'))
 })
